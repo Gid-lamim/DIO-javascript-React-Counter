aa4800b72f8061ce3c7c9d859b13611c4a8099d3
Gid.Lamim <gid.lamim@gmail.com>
2022-07-09 09:10:42 -0300

    use useState
    here we've used useState to change the state of the component.

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,19 +1,12 @@
+import {useState} from 'react';
+
 function Counter () {
-    let number = 10;
+    
+    const [number, changeNumber] = useState(1);
 
-    function addNumber(){
-        number = number + 1;
-        document.getElementById("counter-box").innerHTML = number;
-
-    }
-    function subtractNumber(){
-        number = number - 1;
-        document.getElementById("counter-box").innerHTML = number;
-
-    }
     return <div>
                 <h2 id="counter-box">{number}</h2>
-                <div><button onClick={addNumber} ig="a">Increase</button><button onClick={subtractNumber}>Decrease</button></div>
+                <div><button onClick={()=> changeNumber(number + 1)} >Increase</button><button onClick={()=> changeNumber(number - 1)}>Decrease</button></div>
 
             </div>
 }
